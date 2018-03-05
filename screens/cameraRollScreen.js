e30657a2ea8497e7f966bc322436961a6aaa94d6
Evan Norton <evanan39@yahoo.com>
2018-03-05 14:54:27 -0600

    getImage function

--- screens/cameraRollScreen.js
+++ screens/cameraRollScreen.js
@@ -3,15 +3,9 @@ import { ScrollView } from 'react-native';
 
 import CameraRollPicker from 'react-native-camera-roll-picker';
 
+import { post } from '../services/rest';
+
 export default class CameraRollScreen extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            url: ''
-        }
-    }
 
     static navigationOptions = {
         headerLeft: null,
@@ -25,10 +19,32 @@ export default class CameraRollScreen extends Component {
         }
     };
 
+    getImage(images) {
+        let uri = images[0].uri;
+        const data = new FormData();
+
+        /*
+        data.append('name', 'testName'); // you can append anyone.
+        data.append('image', {
+            uri: image.uri,
+            type: 'image/jpeg', // or photo.type
+            name: 'testPhotoName'
+        });
+        fetch(url, {
+            method: 'post',
+            body: data
+        }).then(res => {
+            console.log(res)
+        });
+        post('/api/images', )
+        */
+
+    }
+
     render() {
         return (
             <CameraRollPicker
-                callback={this.getSelectedImages}
+                callback={this.getImage}
                 maximum={1}
             />
         );
