8b12decdf3322cb6c3a6dd2355ecf56f5b366c05
Dustin Pierson <dustinpierson@Dustins-MacBook-Pro.local>
2018-03-12 10:36:48 -0500

    calendarScreen

--- screens/calendarScreen.js
+++ screens/calendarScreen.js
@@ -3,12 +3,18 @@ import { View, StyleSheet, Image, Text } from 'react-native';
 import {Calendar, CalenderList, Agenda} from 'react-native-calendars'
 import { Button } from 'react-native-elements';
 import { DEFAULT_NAVIGATION_OPTIONS } from '../services/navigation';
+import { getMe } from '../services/user';
+
 
 
 export default class CalendarScreen extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            me: {},
+        }
     }
 
     static navigationOptions = {
@@ -21,19 +27,50 @@ export default class CalendarScreen extends Component {
 
     }
 
+    async componentDidMount(){
+        let me = await getMe();
+        this.setState({
+            me,
+        });
+    }
+
     navigate(screen) {
         userType = this.props.navigation.state.params.userType;
         this.props.navigation.navigate(screen, { userType });
-    }
+    };
+
+    renderItem(item) {
+        return (
+          <View style={styles.item}><Text>{item.text.usertype} {item.text.name}</Text></View>
+        );
+      }
+    
+      renderEmptyDate() {
+        return (
+          <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+        );
+      }
 
     render() {
 
-        if (this.props.screenProps.userType === 'Mentor') {
+        if (this.state.me.usertype === 'Mentor') {
             return (
                 <View style={styles.container}>
                     <Text style={styles.text}>Mentor Calendar Screen</Text>
-                    {/* <Calendar /> */}
-                    <Agenda />
+                    <Agenda 
+                    items={
+                        {'2018-03-12': [],
+                         '2018-03-13': [],
+                         '2018-03-14': [],
+                         '2018-03-15': [],
+                         '2018-03-16': [{text:this.state.me}],
+                        }}
+                        renderItem={this.renderItem.bind(this)}
+                        renderEmptyDate={this.renderEmptyDate.bind(this)}
+                       
+                    
+                        rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+                    />
                 </View>
             );
 
@@ -41,8 +78,18 @@ export default class CalendarScreen extends Component {
             return (
                 <View style={styles.container}>
                     <Text style={styles.text}>Student Calendar Screen</Text>
-                    {/* <Calendar /> */}
-                    <Agenda />
+                    <Agenda 
+                    items={
+                        {'2018-03-12': [],
+                         '2018-03-13': [],
+                         '2018-03-14': [],
+                         '2018-03-15': [],
+                         '2018-03-16': [{text:this.state.me}],
+                        }}
+                        renderItem={this.renderItem.bind(this)}
+                        renderEmptyDate={this.renderEmptyDate.bind(this)}
+                        rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+                    />
                 </View>
             )
         }
@@ -52,13 +99,27 @@ export default class CalendarScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         padding: 30,
-        backgroundColor: 'blue',
+        backgroundColor: 'white',
         flex: 1,
         justifyContent: 'center'
     },
 
     text: {
         textAlign: 'center'
-    }
+    },
+    item: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17,
+        height: 15,
+      },
+      emptyDate: {
+        height: 15,
+        flex:1,
+        paddingTop: 30
+      }
 
 });
