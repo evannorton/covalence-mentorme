737441f5d47da9a2e9c29c07c88966f4e0a10fc2
Dustin Pierson <dustinapierson@gmail.com>
2018-03-15 10:18:15 -0500

    added getAppointments

--- services/calendar.js
+++ services/calendar.js
@@ -11,5 +11,13 @@ async function addException(id, exceptions) {
 async function createAppointment(date, mentorid, studentid, subjectid, hour) {
     return await RestServices.post('/api/appointments/', { date, mentorid, studentid, subjectid, hour });
 }
+async function getAppointments(usertype,id,confirmed){
+    if (usertype === 'Mentor') {
+        return await RestServices.get(`/api/appointments/mentors/${id}/${confirmed}`);
+    } else {
+        return await RestServices.get(`/api/appointments/students/${id}/${confirmed}`);
+    }
+    
+}
 
-export { getAvailability, addException, createAppointment }
+export { getAvailability, addException, createAppointment,getAppointments }
