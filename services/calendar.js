3a43db91f6de085ed3809b2a84a0eb6c6d67b107
Evan Norton <evanan39@yahoo.com>
2018-03-16 10:52:37 -0500

    remove exception when appointment is confirmed

--- services/calendar.js
+++ services/calendar.js
@@ -10,6 +10,21 @@ async function addException(id, exceptions) {
     return await RestServices.put(`/api/availability/${id}`, { exceptions });
 }
 
+async function removeException(id, exceptions, exception) {
+    exception = exception.substring(0, exception.length - 2);
+    console.log(arguments);
+    let index = exceptions.indexOf(exception);
+    if (index === -1) {
+        return;
+    } else if (index === 0) {
+        exceptions = exceptions.substring(exception.length + 1);
+    } else {
+        exceptions = exceptions.substring(0, index - 1) + exceptions.substring(index + exception.length);
+    }
+    console.log(exceptions);
+    return await RestServices.put(`/api/availability/${id}`, { exceptions });
+}
+
 async function getAppointments(usertype, userid, confirmed) {
     if (usertype === 'Mentor') {
         return await RestServices.get(`/api/appointments/mentors/${userid}/${confirmed}`);
@@ -30,4 +45,4 @@ async function deleteAppointment(id) {
     return await RestServices.destroy(`/api/appointments/${id}`);
 }
 
-export { getAvailability, addException, getAppointments, createAppointment, confirmAppointment, deleteAppointment };
+export { getAvailability, addException, removeException, getAppointments, createAppointment, confirmAppointment, deleteAppointment };
